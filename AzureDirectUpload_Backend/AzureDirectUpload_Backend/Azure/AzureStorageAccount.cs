591b820fd20842925e6b9cff63fbf0b4f887a1fd
Andrei-FunnyWorld <op4cl@yandex.ru>
2023-11-28 21:12:27 +0700

    Add Backend and jQuery Client

---
+++ AzureDirectUpload_Backend/AzureDirectUpload_Backend/Azure/AzureStorageAccount.cs
@@ -0,0 +1,36 @@
+﻿namespace AzureDirectUpload_Backend.Azure {
+
+    public class AzureStorageAccount {
+        const string AccountNameSetting = "AccountName";
+        const string AccessKeySetting = "AccessKey";
+        const string FileManagerBlobContainerNameSetting = "FileManagerBlobContainerName";
+        const string FileUploaderBlobContainerNameSetting = "FileUploaderBlobContainerName";
+
+        public static AzureStorageAccount FileManager { get; private set; }
+        public static AzureStorageAccount FileUploader { get; private set; }
+
+        public static void Load(IConfigurationSection config) {
+            FileManager = new AzureStorageAccount
+            {
+                AccountName = config[AccountNameSetting],
+                AccessKey = config[AccessKeySetting],
+                ContainerName = config[FileManagerBlobContainerNameSetting]
+            };
+
+            FileUploader = new AzureStorageAccount
+            {
+                AccountName = config[AccountNameSetting],
+                AccessKey = config[AccessKeySetting],
+                ContainerName = config[FileUploaderBlobContainerNameSetting]
+            };
+        }
+
+        public string AccountName { get; set; }
+        public string AccessKey { get; set; }
+        public string ContainerName { get; set; }
+
+        public bool IsEmpty() {
+            return string.IsNullOrEmpty(AccountName) || string.IsNullOrEmpty(AccessKey) || string.IsNullOrEmpty(ContainerName);
+        }
+    }
+}
