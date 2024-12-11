03d620050807fa4b091f1bf1f476a0b9277a10ff
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2024-12-11 18:02:43 +0200

    Update test-example.ps1

--- test-example.ps1
+++ test-example.ps1
@@ -1,18 +1,11 @@
-param (
-    [string]$version = "latest"
-)
-if (-not $env:buildVersion) {
-    $global:buildVersion = $version
-} else {
-    $global:buildVersion = $env:buildVersion
-}
-Write-Host "Build: $inputVersion"
+Write-Output "Branch name: $env:branchName"
 $global:inputVersion = $env:branchName
-$global:errorCode = 0
+$global:LASTEXITCODE = 0
 
 $BUILD_VERSIONS_LIST = "BUILD_VERSIONS_LIST"
 
 $allVersions = @(
+    "14.1", "14.2",
     "15.1", "15.2",
     "16.1", "16.2",
     "17.1", "17.2",
@@ -22,7 +15,8 @@ $allVersions = @(
     "21.1", "21.2",
     "22.1", "22.2",
     "23.1", "23.2",
-    "24.1", "24.2"
+    "24.1", "24.2",
+    "25.1", "25.2" 
 )
 
 function Process-JavaScriptProjects {
@@ -47,16 +41,12 @@ function Process-JavaScriptProjects {
         
         Set-Location $($folder.Name)
 
-		# Prepare the list of packages with their versions
 		$packages = $folder.Packages | ForEach-Object { "$_@$global:buildVersion" }
 
-		# Join the package list into a single string
 		$packageList = $packages -join " "
 
-		# Construct the npm install command
 		$command = "npm install $packageList --force --save --no-fund"
 
-		# Output and execute the command
 		Write-Output "Running: $command"
 		Invoke-Expression $command
 		
@@ -64,7 +54,7 @@ function Process-JavaScriptProjects {
         $installResult = & npm install --force --no-fund --loglevel=error -PassThru
         if ($LASTEXITCODE -ne 0) {
             Write-Error "ERROR: npm install failed in $($folder.Name)"
-            $global:errorCode = 1
+            $global:LASTEXITCODE = 1
         }
 
         Write-Host "`n<-- Updating packages... -->"
@@ -73,7 +63,7 @@ function Process-JavaScriptProjects {
         $buildResult = & npm run build
         if ($LASTEXITCODE -ne 0) {
             Write-Error "ERROR: npm run build failed in $($folder.Name)"
-            $global:errorCode = 1
+            $global:LASTEXITCODE = 1
         }
 
         Set-Location ..
@@ -89,8 +79,7 @@ function Process-DotNetProjects {
     $slnFiles = Get-ChildItem -Path $RootDirectory -Filter *.sln -Recurse -Depth 1
 
     if ($slnFiles.Count -eq 0) {
-        Write-Host "No solution files (.sln) found in the specified directory at level 1."
-        $global:errorCode = 0
+        Write-Host "No solution files (.sln) found in the specified directory at level 1."        
         return
     }
 
@@ -103,7 +92,7 @@ function Process-DotNetProjects {
             Write-Host "Build succeeded for $($slnFile.FullName)."
         } else {
             Write-Error "Build failed for $($slnFile.FullName)."
-            $global:errorCode = 1
+            $global:LASTEXITCODE = 1
         }
     }
 } 
@@ -127,11 +116,13 @@ function Set-BuildVersion {
         $inputMajorMinor = $currentList[0]
         Write-Output "Input version: '$inputMajorMinor'"
         $global:buildVersion = $inputMajorMinor
-
         $updatedList = $currentList[1..($currentList.Count - 1)]
     } else {
-        Write-Output "The list in the environment variable is empty or has only one item. Resetting to the filtered list."
-        $updatedList = $filteredList
+        Write-Output "The list in the environment variable has only one item. "
+        $inputMajorMinor = $currentList
+        $global:buildVersion = $inputMajorMinor
+        Write-Output "Input version: '$inputMajorMinor'"
+        $updatedList = ""
     }
 
     $newValue = $updatedList -join ";"
@@ -148,6 +139,6 @@ Set-BuildVersion
 Process-JavaScriptProjects
 Process-DotNetProjects
 
-Write-Host "Error code: $global:errorCode"
+Write-Host "Error code: $global:LASTEXITCODE"
 
-exit $global:errorCode
+exit $global:LASTEXITCODE
