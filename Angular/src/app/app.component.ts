9dd36873d551cbed7da83309a6056215e94115f3
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-27 16:53:25 +0300

    Fix CI: prettier HTML, missing imports, optional catch binding, stale jQuery job

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 
